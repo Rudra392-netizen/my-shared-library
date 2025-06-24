fea3256c020c0e76303d5814cfb30ccd26f56b44
Rudratech <vir123ender@gmail.com>
2025-06-24 17:16:15 +0530

    Successfully finded the error

--- vars/runSonarQube.groovy
+++ vars/runSonarQube.groovy
@@ -1,19 +1,18 @@
 def call(String sonarCredsId, String sonarUrl, String projectKey) {
-    stage('SonarQube Analysis') {
-        withSonarQubeEnv('SonarQube') {
-            withCredentials([usernamePassword(
-                credentialsId: sonarCredsId,
-                usernameVariable: 'SONAR_USER',
-                passwordVariable: 'SONAR_PASS'
-            )]) {
-                sh """
-                    mvn sonar:sonar \
-                    -Dsonar.login=$SONAR_USER \
-                    -Dsonar.password=$SONAR_PASS \
-                    -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.projectKey=${projectKey}
-                """
-            }
+    withSonarQubeEnv('SonarQube') {
+        withCredentials([usernamePassword(
+            credentialsId: sonarCredsId,
+            usernameVariable: 'SONAR_USER',
+            passwordVariable: 'SONAR_PASS'
+           )]) {
+               sh """
+                mvn sonar:sonar \
+                -Dsonar.login=$SONAR_USER \
+                -Dsonar.password=$SONAR_PASS \
+                -Dsonar.host.url=${sonarUrl} \
+                -Dsonar.projectKey=${projectKey}
+            """
         }
     }
-}
+ }
+
