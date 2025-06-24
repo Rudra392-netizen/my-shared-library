6886e67a36db18f89b3a56f662eae7a118b1513a
Rudratech <vir123ender@gmail.com>
2025-06-24 17:26:27 +0530

    changes have been implemented

--- vars/runSonarQube.groovy
+++ vars/runSonarQube.groovy
@@ -1,14 +1,12 @@
 def call(String sonarCredsId, String sonarUrl, String projectKey) {
-    stage('SonarQube Analysis') {
-        withSonarQubeEnv('SonarQube') {
-            withCredentials([string(credentialsId: sonarCredsId, variable: 'SONAR_TOKEN')]) {
-                sh """
-                    mvn sonar:sonar \
-                    -Dsonar.login=$SONAR_TOKEN \
-                    -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.projectKey=${projectKey}
-                """
-            }
+    withSonarQubeEnv('SonarQube') {
+        withCredentials([string(credentialsId: sonarCredsId, variable: 'SONAR_TOKEN')]) {
+            sh """
+                mvn sonar:sonar \
+                -Dsonar.login=$SONAR_TOKEN \
+                -Dsonar.host.url=${sonarUrl} \
+                -Dsonar.projectKey=${projectKey}
+            """
         }
     }
 }
