256b3e040d6fe95bf302ff2d9fbbd0c0e6b0d03c
Rudratech <vir123ender@gmail.com>
2025-06-24 18:52:12 +0530

    sonarqube file is updated

--- vars/runSonarQube.groovy
+++ vars/runSonarQube.groovy
@@ -1,5 +1,5 @@
-def call(String sonarCredsId, String sonarUrl, String projectKey) {
-    withSonarQubeEnv('SonarQube') {
+def call(String sonarCredsId, String sonarServerName, String sonarUrl, String projectKey) {
+    withSonarQubeEnv(sonarServerName) {
         withCredentials([string(credentialsId: sonarCredsId, variable: 'SONAR_TOKEN')]) {
             sh """
                 mvn sonar:sonar \
@@ -10,4 +10,3 @@ def call(String sonarCredsId, String sonarUrl, String projectKey) {
         }
     }
 }
-
