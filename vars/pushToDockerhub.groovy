5629949afebf7e5a238f804eeb343340f9b81f97
Rudratech <vir123ender@gmail.com>
2025-06-25 10:51:31 +0530

    updated file

--- vars/pushToDockerhub.groovy
+++ vars/pushToDockerhub.groovy
@@ -1,15 +1,13 @@
 def call(String IMAGE_NAME, String dockerHubCred) {
-    stage("Push to dockerhub") {
-        withCredentials([usernamePassword(
-            credentialsId: dockerHubCred,
-            usernameVariable: "DOCKER_USER",
-            passwordVariable: "DOCKER_PASS"
-        )]) {
-            sh """
-                docker login -u $DOCKER_USER -p $DOCKER_PASS
-                docker push ${IMAGE_NAME}
-                docker logout
-            """
-        }
-    }
+    withCredentials([usernamePassword(
+        credentialsId: dockerHubCred,
+        usernameVariable: "DOCKER_USER",
+        passwordVariable: "DOCKER_PASS"
+   )]) {
+        sh """
+            docker login -u $DOCKER_USER -p $DOCKER_PASS
+            docker push ${IMAGE_NAME}
+            docker logout
+        """
+   } 
 }
