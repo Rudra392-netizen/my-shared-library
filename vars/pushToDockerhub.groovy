12f3b899c1d79ab2a1c31a9c2b2e20fe5e14f41f
Rudratech <vir123ender@gmail.com>
2025-06-25 10:47:31 +0530

    updated file

--- vars/pushToDockerhub.groovy
+++ vars/pushToDockerhub.groovy
@@ -4,10 +4,12 @@ def call(String IMAGE_NAME, String dockerHubCred) {
             credentialsId: dockerHubCred,
             usernameVariable: "DOCKER_USER",
             passwordVariable: "DOCKER_PASS"
-        )])
-        sh """
-        docker login -u $DOCKER_USER -p $DOCKER_PASS
-        docker push ${IMAGE_NAME}
-        """
+        )]) {
+            sh """
+                docker login -u $DOCKER_USER -p $DOCKER_PASS
+                docker push ${IMAGE_NAME}
+                docker logout
+            """
+        }
     }
 }
