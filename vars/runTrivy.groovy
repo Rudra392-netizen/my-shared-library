2c661218dbb2d50d9d71e3d383da1403c9a022d4
Rudratech <vir123ender@gmail.com>
2025-06-25 10:39:23 +0530

    updated file

--- vars/runTrivy.groovy
+++ vars/runTrivy.groovy
@@ -1,5 +1,3 @@
-def call(String IMAGE_NAME) {
-    stage("Trivy scan") {
-        sh "trivy image ${IMAGE_NAME}>trivy.report.txt"
-    }
-}
+def call(String IMAGE_NAME) {   
+    sh "trivy image ${IMAGE_NAME}>trivy.report.txt"
+   }
