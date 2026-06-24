9200b7d3b8dba549f88441b3d37d68b6e4e898b9
Rudra <rpsingh98188@gmail.com>
2026-06-24 11:58:23 +0000

    k8deploy file is updated

--- vars/k8deploy.groovy
+++ vars/k8deploy.groovy
@@ -1,9 +1,9 @@
 def call(String imageTag) {
 
     sh """
-    sed -i 's|IMAGE_TAG|${imageTag}|g' k8s/backend.yaml
+    sed -i 's|IMAGE_TAG|${imageTag}|g' K8s/backend.yaml
 
-    sed -i 's|IMAGE_TAG|${imageTag}|g' k8s/frontend.yaml
+    sed -i 's|IMAGE_TAG|${imageTag}|g' K8s/frontend.yaml
 
     kubectl apply -f k8s/
     """
