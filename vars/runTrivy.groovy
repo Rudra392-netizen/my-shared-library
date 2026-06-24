e8b7eaf106f5ab2f4ef837430b9e2ff7b326affc
Rudra <rpsingh98188@gmail.com>
2026-06-24 11:39:42 +0000

    trivy file is updated

--- vars/runTrivy.groovy
+++ vars/runTrivy.groovy
@@ -1,5 +1,5 @@
-def call(String IMAGE_NAME) {
+def call(String IMAGE_NAME, String image_tag) {
     sh """
-        trivy image --timeout 10m ${IMAGE_NAME} > trivy.report.txt
+        trivy image --timeout 10m ${IMAGE_NAME}:${image_tag} > trivy.report.txt
     """
 }
